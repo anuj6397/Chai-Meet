4b395190368b59548b7b9cf558a6ca7177279319
Anuj Rajput <21bcs4654@cuchd.in>
2025-05-02 08:16:44 +0530

    Update Question.jsx

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -12,7 +12,7 @@ export default function Question() {
   console.log(answer)
  async function generateAnswer(){
          console.log("loding....")
-  const response = await axios({url:"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyAjJYVWix7gT1sg5rRCHX-IfmqLUbisfbo",
+  const response = await axios({url:"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyAjJYVWix7gT1sg5rRCHX-IfmqLUbisfbll",
   method:"post" ,
   data: {
     "contents": [
